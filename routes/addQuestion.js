9abf29d39378ecfa18aaada9d3e22ec9b788d211
Soh Zhe Hong <sohzhehong09@gmail.com>
2024-08-04 20:54:38 +0800

    Changed SQL query for add question operation

--- routes/addQuestion.js
+++ routes/addQuestion.js
@@ -6,9 +6,9 @@ module.exports = (db) => {
         res.render('addQuestion');
     });
     router.post('/addQues', (req, res) => {
-        const {question, cAns, wAns1, wAns2, wAns3, adminid } = req.body;
-        const query = 'INSERT INTO Question_Bank (question, correct_ans, incorrect_ans1, incorrect_ans2, incorrect_ans3, admin_id) VALUES (?, ?, ?, ?, ?)';
-        db.query(query, [question, cAns, wAns1, wAns2, wAns3, adminid], (err, results) => {
+        const {ques, ans1, ans2, ans3, ans4} = req.body;
+        const query = 'INSERT INTO question_bank (question, correct_ans, incorrect_ans1, incorrect_ans2, incorrect_ans3) VALUES (?, ?, ?, ?, ?)';
+        db.query(query, [ques, ans1, ans2, ans3, ans4], (err, results) => {
             if (err) throw err;
             res.redirect('/viewQuestion');
         });
