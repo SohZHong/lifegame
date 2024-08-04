af8530c4a13033e623a566f0138078ba2537e40d
Soh Zhe Hong <sohzhehong09@gmail.com>
2024-08-04 21:03:05 +0800

    Changed naming

--- routes/editQuestion.js
+++ routes/editQuestion.js
@@ -3,16 +3,15 @@ const router = express.Router();
 
 module.exports = (db) => {
     router.get('/editQuestion', (req, res) => {
-        res.render('editQuestion')
+        res.render('editQuestion', { 'id': req.query.questionid})
     });
     router.post('/submitQues', (req, res) => {
-        const {question, cAns, wAns1, wAns2, wAns3, questionID} = req.body;
-        const query = 'UPDATE Question_Bank SET question = ?, correct_ans = ?, incorrect_ans1 = ?, incorrect_ans2 = ?, incorrect_ans3 = ? WHERE question_id = ?;';
-        db.query(query, [question, cAns, wAns1, wAns2, wAns3, questionID], (err, results) => {
+        const {ques, quesId, ans1, ans2, ans3, ans4} = req.body;
+        const query = 'UPDATE question_bank SET question = ?, correct_ans = ?, incorrect_ans1 = ?, incorrect_ans2 = ?, incorrect_ans3 = ? WHERE question_id = ?;';
+        db.query(query, [ques, ans1, ans2, ans3, ans4, quesId], (err, results) => {
             if (err) throw err;
             res.redirect('/viewQuestion');
         });
     });
     return router;
 };
-
